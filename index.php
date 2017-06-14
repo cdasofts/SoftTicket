afc4812de0fd2fc479e73542a2cdc7656dfcdb4e
cdasofts <cdasoft1@gmail.com>
2017-06-13 23:03:57 -0500

    Cambie rutas por unas relativas

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
-        <link rel="stylesheet" href="softicket/plugin/LightBox/css/animate.min.css">
+        <link rel="stylesheet" href="plugin/LightBox/css/animate.min.css">
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-        <script src="softicket/plugin/LightBox/js/animatedModal.min.js"></script>
+        <script src="plugin/LightBox/js/animatedModal.min.js"></script>
 
 
 <!--Font-awsome-->
@@ -25,17 +25,17 @@
 	
 	@font-face {
     font-family: gotham;
-    src: url(softicket/assets/fonts/Gotham.otf);
+    src: url(assets/fonts/Gotham.otf);
 }
 
 @font-face {
     font-family: gotcha;
-    src: url(softicket/assets/fonts/Gotcha.ttf);
+    src: url(assets/fonts/Gotcha.ttf);
 }
 
 @font-face {
     font-family: coco;
-    src: url(softicket/assets/fonts/Coco.ttf);
+    src: url(assets/fonts/Coco.ttf);
 }
 
 </style>
